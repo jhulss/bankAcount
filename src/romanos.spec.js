7db41eef9c2730c0a88b98291009a2404ebe0d6f
Israel Antezana Rojas <israelantezana@gmail.com>
2023-03-18 11:41:29 -0400

    Refactoring duplicacion en test

--- src/romanos.spec.js
+++ src/romanos.spec.js
@@ -1,12 +1,11 @@
 import Romanos from "./romanos.js";
 describe("Romanos", () => {
+  let romanos = new Romanos();
   it("convierte 1 a romano", () => {
-    let romanos = new Romanos();
     expect(romanos.generar(1)).toEqual("I");
   });
 
   it("convierte 2 a romano", () => {
-    let romanos = new Romanos();
     expect(romanos.generar(2)).toEqual("II");
   });
 });
